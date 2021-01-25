34254db2e53daffb0a6199c1cf15b61617c41fd6
beerflaschebrunzer <beerflaschebrunzer@protonmail.com>
2021-01-25 18:23:53 +0100

    better logger

--- core/Logger.php
+++ core/Logger.php
@@ -1,10 +1,29 @@
 <?php
 
-
 class Logger
 {
-    public function info($sContent)
+    public function write($sContent)
     {
         echo date("[Y-m-d H:i:s]") . " " .$sContent . "\n";
+    }
+
+    public function info($sContent)
+    {
+        $this->write(" \t[INFO]\t ".$sContent);
+    }
+
+    public function success($sContent)
+    {
+        $this->write(" \t[SUCCESS]\t ".$sContent);
+    }
+
+    public function warning($sContent)
+    {
+        $this->write(" \t[WARNING]\t ".$sContent);
+    }
+
+    public function error($sContent)
+    {
+        $this->write(" \t[ERROR]\t ".$sContent);
     }
 }
